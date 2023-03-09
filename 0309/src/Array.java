c325d539a21bdbe5f551a05d7ad58daaf50e973f
kw <kw@sen.kr>
2023-03-09 10:59:18 +0900

    Array.java 완성

---
+++ 0309/src/Array.java
@@ -0,0 +1,17 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] num = {94, 85, 95, 88, 90};
+		int max = num[0];
+		
+		for(int i=1; i<num.length; i++) {
+			if(num[i]>max) {
+				max = num[i];
+			}
+		}
+		System.out.println("최대값: "+max);
+	}
+
+}
