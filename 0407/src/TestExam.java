864c94af702a61d210e957e15ddfcbc6572436d5
lkiwon <111848789+lkiwon@users.noreply.github.com>
2023-04-07 10:07:18 +0900

    Update TestExam.java

--- 0407/src/TestExam.java
+++ 0407/src/TestExam.java
@@ -5,7 +5,8 @@ public class TestExam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("500 이하의 자연수를 입력하세요.");
+		int a = sc.nextInt();
 	}
 
 }
